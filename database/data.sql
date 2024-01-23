fd55ad68914d6f144355f8c165e07acbc5fe81d6
Daniel Hahn <danhahn1996@gmail.com>
2024-01-23 19:41:50 +0000

    Initial commit.

--- database/data.sql
+++ database/data.sql
@@ -1,5 +1,4 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
+-- Use SQL insert statements to add any starting/dummy data to your database tables
 
 -- EXAMPLE:
 
@@ -9,3 +8,5 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+insert into "calendar" ("date", "time") values ('01202024', '0900'), ('01252024', '1200'), ('02052024', '1300');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,24 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "calendar" (
+  "calendarId" serial PRIMARY KEY,
+  "userId" integer,
+  "date" integer,
+  "time" integer,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "users" (
+  "userId" integer PRIMARY KEY,
+  "firstName" text,
+  "lastName" text,
+  "username" text,
+  "hashedPassword" text,
+  "email" text,
+  "phone" integer,
+  "createdAt" timestamptz
+);
+
+ALTER TABLE "calendar" ADD FOREIGN KEY ("userId") REFERENCES "users" ("userId");
